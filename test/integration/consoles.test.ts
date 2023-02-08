3ffc5067a291b8bc88040aac69dc2ae604aa78b9
Yuri Desideri <yuridesideri12@gmail.com>
2023-02-08 20:04:21 -0300

    test: routes schema setup

---
+++ test/integration/consoles.test.ts
@@ -0,0 +1,13 @@
+import app from '../../src/app';
+import supertest from 'supertest';
+
+const api = supertest(app);
+
+
+describe('GET /consoles', () => {
+    it ('')
+})
+
+describe ('GET /consoles/:id', () => {})
+
+describe('POST /consoles', () => {})
---
+++ test/integration/games.test.ts
@@ -0,0 +1,13 @@
+import app from '../../src/app';
+import supertest from 'supertest';
+
+const api = supertest(app);
+
+
+describe('GET /games', () => {
+    it ('')
+})
+
+describe ('GET /games/:id', () => {})
+
+describe('POST /games', () => {})
